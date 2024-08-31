f1866f12c2d8de803de74bdbd61ff4041d2b8b94
shivaakira12 <shivaakira000@gmail.com>
2024-08-31 14:04:17 +0530

    code

--- admin/script.js
+++ admin/script.js
@@ -2,5 +2,5 @@ const menuBtn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
 menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("active"); // Toggle the active class on the menu
+  menu.classList.toggle("active");
 });
